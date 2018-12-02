22184e33648b91c272428fd8693523dc0ec4ebaa
Gabriela Almeida <gabicfa1996@gmail.com>
2018-12-01 22:46:13 -0200

    adicao de exemplos

--- src/compiler_main.py
+++ src/compiler_main.py
@@ -2,7 +2,7 @@ from compiler_parser import parser
 
 if __name__ == "__main__":
     try:
-        with open('input.arn', 'r') as myfile:
+        with open('exemplos/input4.gab', 'r') as myfile:
             input_file=myfile.read().replace('\n', '')
     except EOFError:
         raise Exception("Nao foi possivel abrir o arquivo")
